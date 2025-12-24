401e2731208d82c4cee32c86169f4d0352ad0353
quyentran <tranduyquyen1234567890@gmail.com>
2025-12-24 11:25:46 +0700

    app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.smarthome',
+  appName: 'com.example.smarthome',
+  webDir: 'dist'
+};
+
+export default config;
--- src/shared/lib/routes.ts
+++ src/shared/lib/routes.ts
@@ -1,4 +1,3 @@
-import { url } from "zod"
 
 const ROUTES = {
   HOME: {
