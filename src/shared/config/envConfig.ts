93554a30438f147cc3cc8a0d2845222728c26c12
quyentran <tranduyquyen1234567890@gmail.com>
2026-01-05 21:20:57 +0700

    deploy

--- src/shared/config/envConfig.ts
+++ src/shared/config/envConfig.ts
@@ -5,7 +5,6 @@ import { z } from 'zod'
 // định nghĩa schema cho biến môi trường
 const envSchema = z.object({
   VITE_API_URL: z.url('Invalid API URL'),
-  VITE_IMAGE_URL: z.url('Invalid Image URL'),
   VITE_ENV: z.enum(['development', 'production', 'test']).default('development'),
   VITE_SOCKET_URL: z.url('Invalid Socket URL'),
 })
@@ -17,7 +16,6 @@ type Env = z.infer<typeof envSchema>
 function validateEnv(): Env {
   const env = {
     VITE_API_URL: import.meta.env.VITE_API_URL,
-    VITE_IMAGE_URL: import.meta.env.VITE_IMAGE_URL,
     VITE_ENV: import.meta.env.VITE_ENV,
     VITE_SOCKET_URL: import.meta.env.VITE_SOCKET_URL
   }
